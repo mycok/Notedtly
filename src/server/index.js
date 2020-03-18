3b376daf463ecb29597dbcba000c94b828869bb2
myko <awesomeme155@gmail.com>
2020-03-18 12:41:55 +0300

    chore: add swagger documentation
    - install swagger-ui-express
    - add a swagger.json file
    - configure swagger-ui-express middleware
    
    [Finishes #171850739]

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,9 @@ import compression from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 
+import swaggerUi from 'swagger-ui-express';
+import documentation from '../../swagger.json';
+
 const typeDefs = gql`
     type Query {
         hello: String
@@ -17,16 +20,19 @@ const resolvers = {
 };
 
 const app = express();
+
 app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(documentation));
+
 const server = new ApolloServer({ typeDefs, resolvers, context: {} });
 server.applyMiddleware({ app, path: '/api' });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.send('We are here!!!!');
 });
 
